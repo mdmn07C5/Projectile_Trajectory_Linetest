70825ccc95b6203e89b0246e396dd904ce5b5cc9
mdmn07C5 <malicdan@protonmail.ch>
2021-05-18 14:17:40 -0400

    TIME TO REFORMAT. PART OF THAT UNITY PROJECT TO TEACH KIDS KINEMATICS" "

---
+++ Scripts/TrajectoryHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryHandler : MonoBehaviour
+{
+    public Vector3 velocity;
+    public int points;
+    public Transform exitPoint;
+    public Transform dotPrefab;
+    private LineRenderer lineRenderer;
+    private Vector3 position;
+    
+    private List<Transform> trajectoryDots = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = points;
+        position = exitPoint.position;
+        // for (int i = 0; i < points; ++i)
+        // {
+        //     Transform dot = Instantiate(dotPrefab, transform.position, Quaternion.identity);
+        //     dot.gameObject.SetActive(false);
+        //     trajectoryDots.Add(dot);
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawTrajectory(velocity);
+    }
+
+    void DrawTrajectory(Vector3 velocity)
+    {   
+        Vector3 position = exitPoint.position;
+        float xpos = position.x;
+        float ypos = position.y;
+        float time = Time.fixedDeltaTime;
+
+        for ( int i = 0; i < points; ++i )
+        {
+            time = Time.fixedDeltaTime;
+            lineRenderer.SetPosition(i, position);
+            velocity = velocity + Physics.gravity * time;
+            xpos = xpos + velocity.x * time;
+            ypos = ypos + velocity.y * time + 0.5f * Physics.gravity.y * Mathf.Pow(time, 2);
+            position = new Vector3(xpos, ypos, 0);
+        }
+        
+    }
+
+
+}
